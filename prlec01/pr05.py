25f35a95c108e7551c02b53d5095b8102a41be00
ggRefugee <gg.refugee@gmail.com>
2021-07-02 23:10:43 +0300

    Упражнение 5 выполнено!

--- prlec01/pr05.py
+++ prlec01/pr05.py
@@ -1,10 +1,10 @@
 import turtle
 
 turtle.shape('turtle')
-for i in range(5, 51, 5):
+for i in range(0, 10, 1):
     turtle.pendown()
     for j in range(0, 4):
-        turtle.forward(10 + i*5)
+        turtle.forward(40 + 2*i*10)
         turtle.left(90)
     turtle.penup()
     turtle.backward(10)
