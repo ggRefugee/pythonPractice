dcf4ce5ac2e9d4679454e9290c055f78ffde108b
ggrefugee <gg.refugee@gmail.com>
2021-07-02 13:53:00 +0300

    Урпажнения 3 и 4. Нумерация сбилась. упражнения должны были начинаться с 2.

---
+++ prlec01/pr02.py
@@ -0,0 +1,9 @@
+import turtle
+
+turtle.shape('turtle')
+for i in range(1, 5):
+    turtle.forward(90)
+    turtle.left(90)
+
+turtle.mainloop()
+
---
+++ prlec01/pr04.py
@@ -0,0 +1,10 @@
+import turtle
+
+turtle.shape('turtle')
+turtle.speed(50)
+for i in range(1, 361):
+    turtle.forward(2)
+    turtle.left(1)
+
+turtle.mainloop()
+
