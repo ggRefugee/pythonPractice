4a479dd465c395a86816692d79f486e5c9d79145
ggrefugee <gg.refugee@gmail.com>
2021-07-06 18:57:55 +0300

    Упражение 14 работает!!!

--- prlec01/pr14.py
+++ prlec01/pr14.py
@@ -3,28 +3,28 @@ import turtle
 turtle.shape('turtle')
 turtle.speed(3)
 turtle.left(90)
-radius = 30
+radius = 100
 
 def star(n, radius):
     for i in range(1, n+1, 1):
         turtle.forward(radius)
-        turtle.left(90 + (180 / (2 * n)))
+        turtle.left(180 - 360 / (2 * n))
 
 
 turtle.penup()
 turtle.left(90)
-turtle.forward(5 * radius)
+turtle.forward(2 * radius)
 turtle.right(90)
 turtle.pendown()
 turtle.left(90)
 star(5, radius)
 
-"""turtle.penup()
-turtle.right(90)
+turtle.penup()
+turtle.right(180)
 turtle.forward(3 * radius)
-turtle.left(90)
+turtle.left(180)
 turtle.pendown()
-star(11, radius)"""
+star(11, radius)
 
 
 
