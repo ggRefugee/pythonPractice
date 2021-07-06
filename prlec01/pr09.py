434e3172bd62c7b604aa8aa6ca045c84acc6caf3
ggrefugee <gg.refugee@gmail.com>
2021-07-06 13:52:10 +0300

    Упражение 9 по прежнему работает не правильно.

--- prlec01/pr09.py
+++ prlec01/pr09.py
@@ -2,32 +2,30 @@ import turtle
 import math
 
 turtle.shape('turtle')
-
+turtle.left(90)
 n = 3
 distance = 30
-radius = distance / (math.pi / n)
-
 turtle.speed(2)
-#turtle.left(90 / n)
+
 
 def polygon(n, distance):
-    turtle.left(90 / n)
     for i in range(1, n + 1, 1):
         turtle.left(360 / n)
         turtle.forward(distance)
 
 
-while n < 6:
+while True:
     turtle.pendown()
     polygon(n, distance)
-    n += 1
     turtle.penup()
-    radius = distance / (math.pi / n)
-    distance += radius / 3
-    turtle.left(90 - 90 / n)
-    turtle.right(90 / n)
+    radius_out = distance / (2 * math.sin(math.pi / n))
+    n += 1
+    radius_in = distance / (2 * math.tan(math.pi / n))
+    distance = 2 * radius_out * math.tan(math.pi / n)
+    turtle.right(45)
     turtle.forward(10)
-    turtle.left(90)
+    turtle.left(45)
+
 
 
 turtle.mainloop()
